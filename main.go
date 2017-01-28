6c9b57a9ff06086574ecf1374df094f573ca519b
stve <steve.agalloco@gmail.com>
2017-01-27 23:53:01 -0500

    significant rewrite using cli
    * adds urfave/cli dependency
    * add support for --file and --directory flags

--- main.go
+++ main.go
@@ -11,29 +11,91 @@ import (
 
 	"github.com/mitchellh/go-homedir"
 	"github.com/rojters/opengraph"
+	"gopkg.in/urfave/cli.v1"
 )
+
+// Artwork : Struct to store the relevant opengraph metadata for saving the image
+type Artwork struct {
+	Title    string
+	ImageURL string
+}
 
 const defaultDir string = "~/Downloads"
 
 func main() {
-	args := os.Args[1:]
+	var filename string
+	var directory string
+	var url string
 
-	destinationDir, err := homedir.Expand(defaultDir)
-	if err != nil {
-		log.Fatal(err)
+	app := cli.NewApp()
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "filename, file, f",
+			Usage:       "Save image as `FILE`",
+			Value:       "",
+			Destination: &filename,
+		},
+		cli.StringFlag{
+			Name:        "directory, dir, d",
+			Usage:       "Save image to `DIR`",
+			Value:       "",
+			Destination: &directory,
+		},
 	}
 
-	if len(args) <= 0 {
-		Usage()
-		return
+	app.Action = func(context *cli.Context) error {
+		if context.NArg() > 0 {
+			url = context.Args()[0]
+		} else {
+			cli.ShowAppHelp(context)
+			return nil
+		}
+
+		if checkIsEmpty(directory) {
+			directory = defaultDir
+		}
+
+		if directory == "." {
+			wd, err := os.Getwd()
+			if err != nil {
+				log.Fatal("error loading directory")
+			}
+
+			directory = wd
+		}
+
+		destinationDir, err := homedir.Expand(directory)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		metaData := fetchMetaData(url)
+		artwork := extractMetaData(metaData)
+
+		if checkNotEmpty(artwork.Title) && checkNotEmpty(artwork.ImageURL) {
+			if checkIsEmpty(filename) {
+				filename = artwork.Title + ".jpg"
+			}
+
+			if strings.Contains(filename, "//") {
+				filename = strings.Replace(filename, "//", "-", -1)
+			}
+
+			SaveFile(destinationDir, filename, artwork.ImageURL)
+
+			fmt.Println("Downloaded " + filename + " to " + destinationDir)
+		} else {
+			fmt.Println("couldn't locate metadata")
+		}
+
+		return nil
 	}
 
-	url := args[0]
-	if url == "" {
-		Usage()
-		return
-	}
+	app.Run(os.Args)
+}
 
+func fetchMetaData(url string) []opengraph.MetaData {
 	res, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
@@ -44,34 +106,26 @@ func main() {
 		log.Fatal(err)
 	}
 
-	var title, imageURL string
+	return md
+}
+
+func extractMetaData(md []opengraph.MetaData) Artwork {
+	var artwork Artwork
 
 	for i := range md {
 		switch md[i].Property {
 		case "title":
-			title = md[i].Content
+			artwork.Title = md[i].Content
 		case "image":
-			imageURL = md[i].Content
+			artwork.ImageURL = md[i].Content
 		}
 	}
 
-	if len(title) > 0 && len(imageURL) > 0 {
-		title = title + ".jpg"
-
-		if strings.Contains(title, "//") {
-			title = strings.Replace(title, "//", "-", -1)
-		}
-
-		SaveFile(destinationDir, title, imageURL)
-
-		fmt.Println("Downloaded " + title + " to " + destinationDir)
-	} else {
-		fmt.Println("couldn't locate metadata")
-	}
+	return artwork
 }
 
 // SaveFile - saves the image to a local file
-func SaveFile(destDir string, title string, imageURL string) {
+func SaveFile(destDir string, filename string, imageURL string) {
 	res, e := http.Get(imageURL)
 	if e != nil {
 		log.Fatal(e)
@@ -79,7 +133,7 @@ func SaveFile(destDir string, title string, imageURL string) {
 
 	defer res.Body.Close()
 
-	file, err := os.Create(path.Join(destDir, title))
+	file, err := os.Create(path.Join(destDir, filename))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -91,10 +145,10 @@ func SaveFile(destDir string, title string, imageURL string) {
 	}
 }
 
-// Usage - Display usage info
-func Usage() {
-	fmt.Println("Please pass a url to check")
-	fmt.Println("")
-	fmt.Println("usage: scar https://soundcloud.com/artist/track")
-	return
+func checkIsEmpty(str string) bool {
+	return (len(str) == 0)
+}
+
+func checkNotEmpty(str string) bool {
+	return (len(str) > 0)
 }
